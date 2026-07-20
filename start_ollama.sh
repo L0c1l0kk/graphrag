b1c2eab07e22990a7743777bd636850b1aef3c70
L0c1l0kk <locilokk@gmail.com>
2026-07-20 21:50:52 +0200

    updated ollama script

--- start_ollama.sh
+++ start_ollama.sh
@@ -3,7 +3,7 @@
 export PATH=$HOME/.local/ollama/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/.local/ollama/lib/ollama:$LD_LIBRARY_PATH
 export OLLAMA_NUM_PARALLEL=4
-export OLLAMA_CONTEXT_LENGTH=2048
+export OLLAMA_CONTEXT_LENGTH=4048
 export LLAMA_ARG_CACHE_RAM=0
 
 nohup ollama serve > $HOME/ollama.log 2>&1 &
